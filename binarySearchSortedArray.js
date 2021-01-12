49beb7beb75e03d1669f731e776a3090fc57db9e
Lim Nhep <limenhep@gmail.com>
2021-01-12 03:23:09 -0800

    Complet an alternative version of binarySearch.js using iterative method

--- binarySearchSortedArray.js
+++ binarySearchSortedArray.js
@@ -12,3 +12,39 @@ let binarySearch = function (a, key) {
   const findIndex = findElement.length === 0 ? -1 : findElement[0][1]; // if the length is 0, then the index isn't existed. therefore, returns -1 and exit out of the function; otherwise, return the first element of the array and its second element.
   return findIndex; // return the index that is found in findIndex.
 };
+
+// Alternative solution using Recursive function
+
+let binarySearch = function (a, key) {
+  let low = 0; // set low as 0 index
+  let high = a.length - 1; // set high as last index
+  while (low <= high) { // as long as low is less than or equal to high
+    let mid = low + Math.floor((high - low) / 2); // get the middle index
+    if (a[mid] === key) { // if the element of the middle index is the same as the key
+      return mid; // then key is found
+    }
+    if (key < a[mid]) { // if key is less than the middle element
+      high = mid - 1; // set high to mid index minus 1
+    } else {
+      low = mid + 1; // otherwise, set low to middle index plus 1
+    }
+    return -1; // if the conditions are not met, return -1 by default
+  }
+};
+
+// Binary Search using iterative method (while loop)
+
+let binarySearch = (a, key) => {
+  let low = 0;
+  let high = a.length - 1;
+  while (low <= high) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (a[mid] === key) return mid;
+    if (key < a[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+    return -1;
+  }
+};
