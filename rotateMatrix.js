5ce7e0effa14e029ce4ed0f3c0ed94c169737444
Lim Nhep <limenhep@gmail.com>
2021-01-15 13:17:27 -0800

    Create rotateMatrix.js

---
+++ rotateMatrix.js
@@ -0,0 +1,6 @@
+// write a function that return a new metrix that represents the original array, but rotated 90 degree clockwise (to the right).
+
+// i: array of arrays of integer
+// o: array of arrays of integer
+
+
