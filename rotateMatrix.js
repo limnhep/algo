e866b7ab61e0f73789e29c1f3e5ec4807bc1b2a0
Lim Nhep <limenhep@gmail.com>
2021-01-15 14:42:31 -0800

    Complete rotateMatrix.js

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -3,4 +3,15 @@
 // i: array of arrays of integer
 // o: array of arrays of integer
 
+const rotateClockwise = (metrix) => {
+  const newMetrix = metrix[0].map(() => []);
+  for (let i = 0; i < metrix.length; i++) {
+    for (j = 0; j < metrix[0].length; j++) {
+      newMetrix[j][metrix.length - 1 - i] = metrix[i][j];
+    }
+  }
+  return newMetrix;
+}
 
+// time: O(n) because each item is being processed once only.
+// space: O(n)
