efd92021bf37a8cc63bde898d2d2042fb203ac3c
Lim Nhep <limenhep@gmail.com>
2021-01-18 07:17:32 -0800

    Complete findSum.js alternative solution

--- findSum.js
+++ findSum.js
@@ -14,3 +14,32 @@ const findSum = (arr, value) => {
   }
   return false;
 }
+
+// time: O(n)
+// space: O(1)
+
+// alternative solution:
+
+const findSum = (arr, value) => {
+  arr.sort((a, b) => a - b);
+  let left = 0,
+    right = arr.length - 1,
+    sum = 0;
+  const result = [];
+  while (left != right) {
+    sum = arr[left] + arr[right];
+    if (sum < value) {
+      left++;
+    } else if (sum > value) {
+      right--;
+    } else {
+      result.push(arr[left]);
+      result.push(arr[right]);
+      return result;
+    }
+  }
+  return false;
+}
+
+// time: O(nlogn)
+// space: O(n)
