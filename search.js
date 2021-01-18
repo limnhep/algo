6ec9ba89314836fad5086522ce6710564c851bd3
Lim Nhep <limenhep@gmail.com>
2021-01-17 17:10:11 -0800

    Complete search.js

--- search.js
+++ search.js
@@ -1,3 +1,6 @@
 const search = (nums, target) = {
-  return true;
+  for(let i = 0; i<nums.length; i++) { // brute force approach where nums is in chronological order and therefore, iterate over each element until we find nums[i]
+    if (num[i] === target) return i; // if nums[i] exists, then return the current element
+  }
+return undefined; // otherwise, return undefined
 }
