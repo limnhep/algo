756236032d4f33af9d3f7f96d2c354ba2d475d30
Lim Nhep <limenhep@gmail.com>
2021-01-23 14:29:57 -0800

    addTotail implementLinkedList.js

---
+++ implementLinkedList.js
@@ -0,0 +1,48 @@
+// Time and Space Complexity of Linked List, for both single and double.
+
+// Unlike Array, Linked List nodes are NOT stored contiguously in memory and do NOT have an indexed set of memory addresses.
+
+// Access =>
+// Time: O(n), linear
+// Space: O(n), linear
+
+// Search =>
+// Time: O(n), linear
+// Space: O(n), linear
+
+// Insertion =>
+// Time: O(1), constant
+// Space: O(n), linear
+
+// Deletion =>
+// Time: O(1), constant
+// Space: O(n), linear
+
+// Implement a singly Linked List and all of its methods.
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToTail(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
