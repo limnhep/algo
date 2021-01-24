0e5d624edc43f6b8f9db5911dc534d7ff96537aa
Lim Nhep <limenhep@gmail.com>
2021-01-24 10:44:45 -0800

    Add removeTail to implementLinkedList.js

--- implementLinkedList.js
+++ implementLinkedList.js
@@ -45,4 +45,23 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  removeTail() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
 }
