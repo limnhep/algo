14a99de844d25dda1e5460a31ef9fb4e01f3cc4d
Lim Nhep <limenhep@gmail.com>
2021-01-27 16:53:29 -0800

    Complete majorityElement.js

---
+++ majorityElement.js
@@ -0,0 +1,23 @@
+/*
+    i: an array of nums
+    o: a number that denotes the majority element
+    e: na, assuming.
+    strategy:
+        1. store each element inside object; if the key exists in the object, increment the value by 1;
+        2. iterate again to identify any value in the object that is greater than initialized majority, which is n/2;
+
+*/
+var majorityElement = function (nums) {
+  const obj = {},
+    majority = nums.length / 2;
+  for (let num of nums) {
+    if (num in obj) {
+      obj[num]++;
+    } else {
+      obj[num] = 1;
+    }
+  }
+  for (let num of nums) {
+    if (obj[num] > majority) return num;
+  }
+};
