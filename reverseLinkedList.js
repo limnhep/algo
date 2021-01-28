2036ffcfc8d7459f887969c8a8472801fe7d5f6d
Lim Nhep <limenhep@gmail.com>
2021-01-27 18:34:32 -0800

    Complete reverseLinkedList.js

---
+++ reverseLinkedList.js
@@ -0,0 +1,21 @@
+/*
+    basic of Linked List: it has a head, a node, a tail. [head, data] => [data, next] => [data, null]
+    i: a singly linked list
+    o: a reverse singly linked list
+    strategy: uses pointer to point to previous and next node as long as the currentNode is not null; because it's null, then it's going to be an end of the linked list.
+*/
+var reverseList = function (head) {
+  let tail = null;
+  let node = head;
+  while (node !== null) { // indicates that it's not an end
+    const nextNode = node.next;
+    node.next = tail;
+    tail = node;
+    node = nextNode;
+
+  }
+  return tail;
+};
+
+// time: O(n)
+// space: O(1)
