3419e528cdd482614dd7ea4dd823623174b19a3a
Lim Nhep <limenhep@gmail.com>
2021-02-08 13:25:07 -0800

    Add different order methods to tree.js

--- tree.js
+++ tree.js
@@ -5,3 +5,30 @@ class TreeNode {
     this.right = null;
   }
 }
+
+// print left subtree, root, and the right subtree
+
+function inOrderPrint(root) {
+  if (root === null) return; // (!root) recursive case
+  inOrderPrint(root.left);
+  console.log(root.val);
+  inOrderPrint(root.right);
+}
+
+// print root, left subtree, and the right subtree
+
+function preOrderPrint(root) {
+  if (!root) return;
+  console.log(root.val);
+  preOrderPrint(root.left);
+  preOrderPrint(root.right);
+}
+
+// print left subtree, right subtree, and then root
+
+function postOrderPrint(root) {
+  if (!root) return;
+  postOrderPrint(root.left);
+  postOrderPrint(root.right);
+  console.log(root.val);
+}
