f4192e294be3ad4e23fdefa210820f021e4fc0bc
Lim Nhep <limenhep@gmail.com>
2021-02-10 15:31:51 -0800

    Complete maxProduct.js

---
+++ maxProduct.js
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+Example 1:
+
+Input: [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let priorMax = nums[0],
+    priorMin = nums[0],
+    result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    let currMax = Math.max(priorMax * nums[i], nums[i], priorMin * nums[i]);
+    let currMin = Math.min(priorMax * nums[i], nums[i], priorMin * nums[i]);
+
+    priorMax = currMax;
+    priorMin = currMin;
+    result = Math.max(currMax, result);
+  }
+  return result;
+};
--- maxSubArray.js
+++ maxSubArray.js
@@ -14,3 +14,22 @@ var maxSubArray = function (nums) {
   }
   return maximum;
 };
+
+// time: O(n)
+// space: O(1)
+
+// alternative solution:
+
+
+var maxSubArray = function (nums) {
+  let currMax = nums[0],
+    max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currMax = Math.max(nums[i], currMax + nums[i]);
+    max = Math.max(max, currMax);
+  }
+  return max;
+};
+
+// time: O(n)
+// space: O(1)
