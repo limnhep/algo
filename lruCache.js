0b0a9e461e9914ba8fb5ec8ed941fedea4fbadef
Lim Nhep <limenhep@gmail.com>
2021-02-11 12:14:43 -0800

    Complete threeSum.js

--- lruCache.js
+++ lruCache.js
@@ -65,3 +65,7 @@ class LRUCache {
     }
   }
 }
+
+// alternative solution:
+
+
--- threeSum.js
+++ threeSum.js
@@ -24,3 +24,41 @@ Constraints:
 0 <= nums.length <= 3000
 -105 <= nums[i] <= 105
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  const triplets = [];
+  let target = 0;
+  if (nums.length < 3) return triplets;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > target) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let start = i + 1,
+      end = nums.length - 1;
+    while (start < end) {
+      let currSum = nums[i] + nums[start] + nums[end];
+      if (currSum === target) {
+        triplets.push([nums[i], nums[start], nums[end]]);
+        while (nums[start] === nums[start + 1]) start++;
+        while (nums[end] === nums[end - 1]) end--;
+        start++;
+        end--;
+        continue;
+      }
+      if (currSum < target) {
+        start++;
+        continue;
+      }
+      if (currSum > target) {
+        end--;
+        continue;
+      }
+    }
+  }
+  return triplets;
+};
+
