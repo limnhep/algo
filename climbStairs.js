0ae1ed63258347714118cf48abaa88cab8c1be11
Lim Nhep <limenhep@gmail.com>
2021-02-16 19:24:41 -0800

    Complete climbStairs.js

--- climbStairs.js
+++ climbStairs.js
@@ -26,4 +26,17 @@ Constraints:
 
 1 <= n <= 45
 */
+var climbStairs = function (n) {
+  if (n === 1) return 1;
+  let dp = [n + 1];
+  console.log('dp: ', dp);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
 
+// time: O(n)
+// space: O(n)
