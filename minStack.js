5c53e69d97822656134ff59962dea22523d3cff4
Lim Nhep <limenhep@gmail.com>
2021-03-05 16:45:57 -0800

    Complete alternative solution to minStack.js

--- minStack.js
+++ minStack.js
@@ -81,3 +81,58 @@ MinStack.prototype.getMin = function () {
 
 // time: O(1) where push / pop is operated at constant time
 // space: O(n) where n is the size of the
+
+// alternative soilution:
+
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this.minStack = [];
+  this.stack = [];
+};
+
+/**
+* @param {number} x
+* @return {void}
+*/
+MinStack.prototype.push = function (x) {
+  this.stack.push(x);
+  if (this.minStack.length === 0 || this.minStack[this.minStack.length - 1] >= x) {
+    this.minStack.push(x);
+  }
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function () {
+  if (this.stack.length === 0) return null;
+  let val = this.stack.pop();
+  if (val === this.minStack[this.minStack.length - 1]) this.minStack.pop();
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function () {
+  if (this.stack.length === 0) return null;
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function () {
+  if (this.minStack.length === 0) return null;
+  return this.minStack[this.minStack.length - 1];
+};
+
+/**
+* Your MinStack object will be instantiated and called as such:
+* var obj = new MinStack()
+* obj.push(x)
+* obj.pop()
+* var param_3 = obj.top()
+* var param_4 = obj.getMin()
+*/
