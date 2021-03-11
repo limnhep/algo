5abd0e8caee9fe532f1fb7df98b9198f70c3fa44
Lim Nhep <limenhep@gmail.com>
2021-03-10 22:01:56 -0800

    Complete subarraySum.js

---
+++ subarraySum.js
@@ -0,0 +1,43 @@
+/*
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  let sum = 0, count = 0;
+  const map = new Map();
+  map.set(0, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    map.set(sum, map.get(sum) + 1 || 1);
+  }
+  return count;
+};
+
+// time: O(n);
+// space: O(n); map contains n distinct entries
