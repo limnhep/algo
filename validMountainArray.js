e3ea3e3423432aef5bfc871f4e247dcc0156d56c
Lim Nhep <limenhep@gmail.com>
2021-03-27 20:54:47 -0700

    Add validMountainArray.js

---
+++ validMountainArray.js
@@ -0,0 +1,52 @@
+/*
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 104
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function (arr) {
+  if (arr.length < 3) return false;
+  let left = 0;
+  let right = arr.length;
+  while (left + 1 < right && arr[left] < arr[left + 1]) { // left + 1 < right, meaning we haven't reached the end
+    left++;
+  }
+  if (left === 0 || left === right - 1) return false;
+  while (left + 1 < right && arr[left] > arr[left + 1]) {
+    left++;
+  }
+  return left === right - 1;
+};
+
+// time: O(n)
+// space: O(1)
