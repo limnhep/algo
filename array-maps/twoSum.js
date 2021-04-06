fc3cd6febae1d65a32707518a07f4c12669bda6c
Lim Nhep <limenhep@gmail.com>
2021-04-06 13:01:51 -0700

    Add twoSum.js II

--- array-maps/twoSum.js
+++ array-maps/twoSum.js
@@ -63,3 +63,50 @@ var twoSum = function (nums, target) {
 
 // time: O(n), where n is the length of the elements inside the array
 // space: O(n), where n is the length of the elements inside the array
+
+
+/*
+Given an array of integers numbers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+
+*/
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  const obj = {};
+  for (let i = 0; i < numbers.length; i++) {
+    const currEl = numbers[i];
+    const potentialPair = target - currEl;
+    if (potentialPair in obj) {
+      return [obj[potentialPair], i + 1];
+    } else {
+      obj[currEl] = i + 1;
+    }
+  }
+};
+
+// time: O(n)
+// space: O(n)
