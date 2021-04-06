d85faa9d3d308440b381cb27560c2500c0cf03c0
Lim Nhep <limenhep@gmail.com>
2021-04-06 10:38:20 -0700

    Add vliadPalindrome.js

---
+++ array-maps/validPalindrome.js
@@ -0,0 +1,39 @@
+/*
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+Note:
+The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+  let j = s.length - 1;
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    if (s[i] !== s[j]) {
+      return isPalindrome(removeOneChar(s, i)) || isPalindrome(removeOneChar(s, j));
+    }
+    j--;
+  }
+  return true;
+};
+
+var isPalindrome = function (s) {
+  return s.split('').reverse().join('') === s;
+}
+
+var removeOneChar = function (s, index) {
+  return s.substr(0, index) + s.substr(index + 1);
+}
+
+// time: O(n)
+// space: O(1)
