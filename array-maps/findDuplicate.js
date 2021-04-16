8f80ffb9a0bb3782f43f75d30a73e38ab55acb17
Lim Nhep <limenhep@gmail.com>
2021-04-16 15:38:54 -0700

    Add findDuplicate.js

---
+++ array-maps/findDuplicate.js
@@ -0,0 +1,49 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [1,1]
+Output: 1
+Example 4:
+
+Input: nums = [1,1,2]
+Output: 1
+
+
+Constraints:
+
+2 <= n <= 3 * 104
+nums.length == n + 1
+1 <= nums[i] <= n
+All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      return nums[i];
+    }
+  }
+};
+
+// time: O(nlogn)
+// space: O(nlogn)
