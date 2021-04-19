de7f61dbcd4db6f332e142bbb45da5d966790d2d
Lim Nhep <limenhep@gmail.com>
2021-04-19 15:29:50 -0700

    Add trap.js

--- trap.js
+++ trap.js
@@ -49,3 +49,38 @@ var trap = function (height) {
 
 // time: O(n^2)
 // space: O(1)
+
+// Alternative solution
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxLeft = 0;
+  let maxRight = 0;
+  let totalWater = 0;
+  while (left < right) {
+    if (height[left] <= height[right]) {
+      if (height[left] >= maxLeft) {
+        maxLeft = height[left];
+      } else {
+        totalWater += maxLeft - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] >= maxRight) {
+        maxRight = height[right];
+      } else {
+        totalWater += maxRight - height[right];
+      }
+      right--;
+    }
+  }
+  return totalWater;
+};
+
+// time: O(n)
+// space: O(1)
