7c5d9d323c4c73e7d23d513ae1d58eb91ef9d233
Lim Nhep <limenhep@gmail.com>
2021-04-26 10:27:40 -0700

    Add lengthOfLongestSubstring.js

---
+++ string/lengthOfLongestSubstring.js
@@ -0,0 +1,54 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4:
+
+Input: s = ""
+Output: 0
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  if (s.length === 0) return 0;
+  let right = 0, left = 0, max = 0;
+  const set = new Set();
+  for (right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+};
+
+// time: O(n)
+// space: O(n)
