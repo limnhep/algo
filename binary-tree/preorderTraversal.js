28b96170b3499db624578e6f901176a538aaf027
Lim Nhep <limenhep@gmail.com>
2021-04-29 22:16:35 -0700

    Add preorderTraversal.js

---
+++ binary-tree/preorderTraversal.js
@@ -0,0 +1,73 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+/*
+    preorderTraversal is starting from root, left subtree, and then right substree.
+    i: [1, null, 2, 3]
+    o: [1, 2, 3]
+*/
+
+var preorderTraversal = function (root) {
+  const result = [];
+  const stack = [];
+  if (root === null) return result;
+  stack.push(root);
+  while (stack.length) {
+    const node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+};
+// time: O(n)
+// space: O(n)
