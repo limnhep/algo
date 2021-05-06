71ac0a1f27243ec2b81626cf8dac1a23c9ae3362
Lim Nhep <limenhep@gmail.com>
2021-05-06 14:07:12 -0700

    Modify postorderTraversal.js

--- depth-first-search/postorderTraversal.js
+++ depth-first-search/postorderTraversal.js
@@ -38,28 +38,3 @@ Follow up:
 
 Recursive solution is trivial, could you do it iteratively?
 */
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-var postorderTraversal = function (root, result = []) {
-  if (!root) return result;
-  if (root !== null) {
-    postorderTraversal(root.left, result);
-    postorderTraversal(root.right, result);
-    result.push(root.val);
-  }
-  return result;
-};
-
-// time: O(n)
-// space: O(1)
