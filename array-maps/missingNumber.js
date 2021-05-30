d0ae3f188229ae696ad4e9fed810cd6e91f3cdca
Lim Nhep <limenhep@gmail.com>
2021-05-29 22:02:07 -0500

    Add missingNumber.js

---
+++ array-maps/missingNumber.js
@@ -0,0 +1,19 @@
+/*
+  You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number ‘x’. You have to find ‘x’. The input array is not sorted.
+*/
+
+let find_missing = function (input) {
+  let actualSum = 0;
+  for (let num of input) actualSum += num;
+  const n = input.length + 1;
+  const sumOfN = Math.floor(n * (n + 1) / 2);
+  return sumOfN - actualSum;
+};
+
+const testArr = [1, 5, 3, 4];
+console.log('The missing number is 2. Therefore, the expected result should be: 2 => ', find_missing(testArr));
+
+/*
+  TIME: O(N) where N is the number of elements in the input array
+  SPACE: O(1)
+*/
