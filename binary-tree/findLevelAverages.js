4b17bcc80496828166640fbacb8acdf1c5d81113
Lim Nhep <limenhep@gmail.com>
2021-06-03 00:24:28 -0500

    Add findLevelAverages.js

---
+++ binary-tree/findLevelAverages.js
@@ -0,0 +1,42 @@
+/*
+  Given a binary tree, populate an array to represent the averages of all of its levels.
+*/
+
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const findLevelAverages = function (root) {
+  const result = [];
+  const queue = [root];
+  if (!root) return result;
+  while (queue.length > 0) {
+    let levelSize = queue.length;
+    let count = 0;
+    let sum = 0;
+    while (count < levelSize) {
+      const currentNode = queue.shift();
+      sum += currentNode.value;
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+      count++;
+    }
+    result.push(sum / levelSize);
+  }
+  return result;
+};
+
+
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(9)
+root.left.right = new TreeNode(2)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+
+console.log(`Level averages are: ${findLevelAverages(root)}`)
--- binary-tree/zigZagTraversal.js
+++ binary-tree/zigZagTraversal.js
@@ -28,3 +28,51 @@ function zigzag_traversal(root) {
 
 // time: O(N)
 // space: O(N) where N is the height of the tree
+
+// alternative solution:
+
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+const traverse = function (root) {
+  const result = [];
+  const queue = [root];
+  let leftToRight = true;
+  if (!root) return result;
+  while (queue.length > 0) {
+    const currentLevel = [];
+    let length = queue.length;
+    let count = 0;
+    while (count < length) {
+      const currentNode = queue.shift();
+      currentLevel.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+      count++;
+    }
+    if (!leftToRight) result.push(currentLevel.reverse());
+    if (leftToRight) result.push(currentLevel);
+    leftToRight = !leftToRight;
+  }
+  return result;
+};
+
+
+var root = new TreeNode(12)
+root.left = new TreeNode(7)
+root.right = new TreeNode(1)
+root.left.left = new TreeNode(9)
+root.right.left = new TreeNode(10)
+root.right.right = new TreeNode(5)
+root.right.left.left = new TreeNode(20)
+root.right.left.right = new TreeNode(17)
+console.log(`Zigzag traversal: ${traverse(root)}`)
+
+// time: O(N) where N is the number of Nodes in the tree
+// space: O(N)
