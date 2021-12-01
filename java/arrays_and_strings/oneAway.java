8bee5853a70178cd25af342365e02bd18cf39a95
Lim Nhep <limenhep@gmail.com>
2021-11-30 18:17:42 -0800

    add oneAway.java

---
+++ java/arrays_and_strings/oneAway.java
@@ -0,0 +1,29 @@
+class OneAway {
+  public static void main(String[] args) {
+    String inputStr1 = "pale";
+    String inputStr2 = "bae";
+    String inputStr3 = "bale";
+    String inputStr4 = "ple";
+    System.out.println("inputStr1 and inputStr2 are false: " + OneAway.oneAway(inputStr1, inputStr2));
+    System.out.println("inputStr2 and inputStr3 are true: " + OneAway.oneAway(inputStr2, inputStr3));
+    System.out.println("inputStr1 and inputStr4 are true: " + OneAway.oneAway(inputStr1, inputStr4));
+  }
+
+  static boolean oneAway(String str1, String str2) {
+    int[] charsStorage = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+    int count = 0;
+    for (char c : str1.toCharArray()) {
+      int index = Character.getNumericValue(c);
+      charsStorage[index]++;
+    }
+    for (char c : str2.toCharArray()) {
+      int index = Character.getNumericValue(c);
+      charsStorage[index]--;
+    }
+    for (int i = 0; i < charsStorage.length; i++) {
+      if (charsStorage[i] > 0)
+        count++;
+    }
+    return count <= 1;
+  }
+}
