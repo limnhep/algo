8fb6435744f5eb43384ec11929b30d7bfee828a4
Lim Nhep <limenhep@gmail.com>
2021-12-02 18:35:18 -0800

    add detectCycle.java

---
+++ java/linked_lists/detectCycle.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+class DetectCycle {
+  public ListNode detectCycle(ListNode head) {
+    Set<ListNode> nodeContaiiners = new HashSet<>();
+    ListNode node = head;
+    while (node != null) {
+      if (nodeContaiiners.contains(node))
+        return node;
+      nodeContaiiners.add(node);
+      node = node.next;
+    }
+    return null;
+  }
+}
+
+// time: O(n) where n is the number of nodes
+// space: O(k) where k is also the number of nodes
