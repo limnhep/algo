952cfbd6efb23f7d43f79bbe4f375fbeb0aa716a
Lim Nhep <limenhep@gmail.com>
2021-12-21 12:29:47 -0800

    Add findAllDuplicates.java

---
+++ java/arrays_and_strings/findAllDuplicates.java
@@ -0,0 +1,48 @@
+/*
+  @request:
+  @param:
+  @return:
+*/
+
+import java.util.ArrayList;
+
+class FindAllDuplicates {
+  public static ArrayList<Integer> findAllDuplicates(int[] nums) {
+    ArrayList<Integer> duplicates = new ArrayList<>();
+    int i = 0;
+    while (i < nums.length) {
+      if (nums[i] != nums[nums[i] - 1]) {
+        swap(nums, i, nums[i] - 1);
+      } else {
+        i++;
+      }
+    }
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[j] == nums[j - 1]) {
+        duplicates.add(nums[j]);
+      }
+    }
+    return duplicates;
+  }
+
+  public static void swap(int[] nums, int i, int j) {
+    int tempNum = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tempNum;
+  }
+
+  public static void main(String[] args) {
+    int[] inputArray = new int[] { 1, 2, 4, 2, 5, 7, 7 };
+    ArrayList<Integer> resultArrList = FindAllDuplicates.findAllDuplicates(inputArray); // Expect 2, 7
+    for (Integer num : resultArrList) {
+      System.out.println(num);
+      System.out.println();
+    }
+  }
+}
+
+/*
+ * Complexity Analysis:
+ * 
+ * Time: O(n) Space: O(1)
+ */
