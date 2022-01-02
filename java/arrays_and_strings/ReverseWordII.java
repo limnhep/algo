f78c35d74666459bc3befb6f5f01435b71cea099
Lim Nhep <limenhep@gmail.com>
2022-01-01 19:08:26 -0800

    Add ReverseWordII.java

---
+++ java/arrays_and_strings/ReverseWordII.java
@@ -0,0 +1,28 @@
+/*
+  @request:
+  @param:
+  @result:
+*/
+
+class ReverseWordII {
+  private static String reverseWord(String str) {
+    StringBuilder word = new StringBuilder();
+    StringBuilder wordsResult = new StringBuilder();
+    for (char eachChar : str.toCharArray()) {
+      if (eachChar != ' ') {
+        word.append(eachChar);
+      } else {
+        wordsResult.append(word.reverse());
+        wordsResult.append(' ');
+        word.setLength(0);
+      }
+    }
+    wordsResult.append(word.reverse());
+    return wordsResult.toString();
+  }
+
+  public static void main(String[] args) {
+    String inputStr = "Let's leetcode!";
+    System.out.println(ReverseWordII.reverseWord(inputStr)); // s'teL !edocteel
+  }
+}
