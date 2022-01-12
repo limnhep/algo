fcb6f6eec5e06871a222bc9bcae59cdb6043bc7e
Lim Nhep <limenhep@gmail.com>
2022-01-11 19:22:36 -0800

    Add findSmallestSubArrayLength.js

---
+++ array-maps/findSmallestSubArrayLength.js
@@ -0,0 +1,26 @@
+(function main() {
+  const findSmallestSubArrayLength = (array, s) => {
+    let right = 0;
+    let left = 0;
+    let sum = 0;
+    let minLen = Number.POSITIVE_INFINITY;
+    for (right = 0; right < array.length; right++) {
+      sum += array[right];
+      while (sum >= s) {
+        minLen = Math.min(minLen, right - left + 1);
+        sum -= array[left];
+        left++;
+      }
+    }
+    return minLen;
+  }
+
+  const arr = [2, 1, 5, 2, 8];
+  const k = 7;
+  console.log(findSmallestSubArrayWithGivenSum(arr, k));
+})()
+
+/*
+  Time: O(n)
+  Space: O(1)
+*/
