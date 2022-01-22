89936b4b64b12752e34ecc8d07c051ae3391a0d3
Lim Nhep <limenhep@gmail.com>
2022-01-21 19:09:39 -0800

    add findMin.js

---
+++ array-maps/findMin.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*
+    @param {number[]} nums
+    @return {number}
+
+    @strategy
+        => find the pivotal point where nums on the right is less than the number on the left
+*/
+var findMin = function (nums) {
+  if (nums.length === 1) return nums[0];
+  let left = 0;
+  let right = nums.length - 1;
+  if (nums[right] > nums[0]) return nums[0]; // implies that the sorted array didn't rotate
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[right] > nums[mid]) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return nums[left];
+};
+
+/*
+  time: O(n)
+  space: O(1)
+*/
