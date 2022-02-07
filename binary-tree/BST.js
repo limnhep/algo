043958f25c89271c29f13b3a96a2caba004ec4bd
Lim Nhep <limenhep@gmail.com>
2022-02-07 10:16:21 -0800

    add BST.js

---
+++ binary-tree/BST.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let temp = this.root;
+    while (temp !== null) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const BSTtree = new BST(); // BST { root: null};
+BSTtree.insert(5);
+BSTtree.insert(3);
+BSTtree.insert(2);
+console.log(BSTtree);
+BSTtree.insert(1);
+BSTtree.insert(4);
+console.log(BSTtree);
